5d9310e2abd5b94ce2f0c1c7455bba07b36fe0fa
aryansamaria <aryansamaria44@gmail.com>
2024-12-09 15:52:11 +0530

    'specific_folder_rendering

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def mustang(request):
+    return render(request,'mustang.html')
---
+++ specific_static_folder/static/js/mustang.js
@@ -0,0 +1,6 @@
+let ro=document.getElementById('root')
+
+let head=document.createElement('h2')
+head.textContent='Oh woowww'
+head.className='roo'
+ro.append(head)
